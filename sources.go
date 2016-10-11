6d1e8ff8bb609dfbeb8da028dfcbdbb7b5af27f5
Nikita <nikita.bokovoy@gmail.com>
2016-10-11 12:46:48 +0300

    Added GitReleases source

--- sources.go
+++ sources.go
@@ -2,6 +2,8 @@ package updater
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -41,14 +43,74 @@ func (dl *DirectLinks) LastVersion() (string, error) {
 }
 
 func (dl *DirectLinks) Download() (string, error) {
-	resp, err := http.Get(dl.DownloadURL)
+	return downloadFrom(dl.DownloadURL, dl.DownloadDir)
+}
+
+const githubAPIURL = "https://api.github.com"
+
+type GitReleases struct {
+	// Credentials is optional and uses only for private repos
+	Username string
+	Password string
+
+	// Repository info
+	Owner string
+	Repo  string
+
+	DownloadURL string
+	DownloadDir string
+}
+
+func (gr *GitReleases) LastVersion() (string, error) {
+	type release struct {
+		Version     string `json:"name"`
+		DownloadURL string `json:"tarball_url"`
+		Description string `json:"body"`
+	}
+
+	lastReleaseURL := fmt.Sprintf("%s/repos/%s/%s/releases/latest", githubAPIURL, gr.Owner, gr.Repo)
+
+	req, err := http.NewRequest(http.MethodGet, lastReleaseURL, nil)
+	if err != nil {
+		return "", err
+	}
+
+	if gr.Username != "" && gr.Password != "" {
+		req.SetBasicAuth(gr.Username, gr.Password)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
 
-	tokens := strings.Split(dl.DownloadURL, "/")
-	fileName := filepath.Join(dl.DownloadDir, tokens[len(tokens)-1])
+	var rel release
+
+	if err := json.NewDecoder(resp.Body).Decode(&rel); err != nil {
+		return "", err
+	}
+
+	if gr.DownloadURL == "" {
+		gr.DownloadURL = rel.DownloadURL
+	}
+
+	return rel.Version, nil
+}
+
+func (gr *GitReleases) Download() (string, error) {
+	return downloadFrom(gr.DownloadURL, gr.DownloadDir)
+}
+
+func downloadFrom(url, toDir string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	tokens := strings.Split(url, "/")
+	fileName := filepath.Join(toDir, tokens[len(tokens)-1])
 
 	file, err := os.Create(fileName)
 	if err != nil {
@@ -56,7 +118,9 @@ func (dl *DirectLinks) Download() (string, error) {
 	}
 	defer file.Close()
 
-	io.Copy(file, resp.Body)
+	if _, err := io.Copy(file, resp.Body); err != nil && err != io.EOF {
+		return "", err
+	}
 
 	return fileName, nil
 }
