4bf9b66571d6bc96a977b1c990b3705ca8da66a0
Nikita <nikita.bokovoy@gmail.com>
2016-10-12 13:57:48 +0300

    Fixed VCSimpleDiff

--- versionCheckers.go
+++ versionCheckers.go
@@ -2,6 +2,10 @@ package updater
 
 func VCSimpleDiff(currentVer string) func(string) bool {
 	return func(ver string) bool {
+		if ver == "" {
+			return false
+		}
+
 		return currentVer != ver
 	}
 }
